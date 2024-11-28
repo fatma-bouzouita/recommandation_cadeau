02c6e2615ec27605fece111d1a5376c611cae318
Emnaghz <emnaghzaiel176@gmail.com>
2024-11-28 14:25:58 +0100

    41-60 ans homme

--- rules/r_41_60.py
+++ rules/r_41_60.py
@@ -168,3 +168,160 @@ class InterfaceEngine_41_60ans(InterfaceEngine):
 
     #homme------------------------------------------------------------------------------------------------------------------
     #anniversaire
+    @Rule(Cadeau(age="41_60ans", sexe="masculin", interest="jardinage", occasion="anniversaire"))
+    def homme_anniversaire_jardinage(self):
+        self.declare(Resultat(result=("Pour un homme aimant le jardinage, voici des idées de cadeaux pour son anniversaire : \n"
+                                        "- Un kit complet d'outils de jardinage (bêche, sécateur, râteau, etc.) \n"
+                                        "- Des gants de jardinage de haute qualité et confortables \n"
+                                        "- Un fauteuil de jardin ou une chaise longue pour se détendre dans son jardin \n"
+                                        "- Des plantes en pot ou des fleurs rares à ajouter à son jardin \n"
+                                        "- Un composteur pour recycler les déchets organiques et nourrir son jardin \n"
+                                        "- Une fontaine de jardin ou un bassin pour ajouter une touche de sérénité \n"
+                                        "- Un abonnement à un magazine de jardinage pour des conseils et des idées")))
+
+    @Rule(Cadeau(age="41_60ans", sexe="masculin", interest="décoration", occasion="anniversaire"))
+    def homme_anniversaire_décoration(self):
+        self.declare(Resultat(result=("Pour un homme aimant la décoration, voici des idées de cadeaux pour son anniversaire : \n"
+                                        "- Une horloge murale design ou vintage pour embellir une pièce \n"
+                                        "- Un tableau ou une œuvre d'art personnalisée correspondant à ses goûts \n"
+                                        "- Une lampe de designer ou une lampe d'ambiance originale \n"
+                                        "- Des coussins décoratifs ou un plaid élégant pour le salon \n"
+                                        "- Un miroir moderne ou au style rétro pour un effet d'agrandissement \n"
+                                        "- Un vase ou une sculpture contemporaine pour la table ou les étagères \n"
+                                        "- Une bibliothèque ou une étagère murale au design unique \n")))
+
+    @Rule(Cadeau(age="41_60ans", sexe="masculin", interest="voyage", occasion="anniversaire"))
+    def homme_anniversaire_voyage(self):
+        self.declare(Resultat(result=("Pour un homme aimant le voyage, voici des idées de cadeaux pour son anniversaire : \n"
+                                        "- Une valise légère et robuste ou un sac de voyage élégant \n"
+                                        "- Une carte du monde à gratter pour marquer les destinations visitées \n"
+                                        "- Un guide de voyage pour sa prochaine destination ou une destination inspirante \n"
+                                        "- Une trousse de toilette compacte et bien organisée pour les déplacements \n"
+                                        "- Un adaptateur universel pour ses voyages à l'étranger \n"
+                                        "- Un oreiller de voyage ergonomique et confortable \n"
+                                        "- Un carnet de voyage personnalisé pour noter ses souvenirs \n"
+                                        "- Une gourde réutilisable ou une bouteille filtrante pour les aventuriers")))
+
+    @Rule(Cadeau(age="41_60ans", sexe="masculin", interest="cuisine", occasion="anniversaire"))
+    def homme_anniversaire_cuisine(self):
+        self.declare(Resultat(result=("Pour un homme aimant la cuisine, voici des idées de cadeaux pour son anniversaire : \n"
+                                        "- Un ensemble de couteaux de cuisine haut de gamme ou personnalisés \n"
+                                        "- Un tablier élégant et personnalisé avec son prénom ou une citation amusante \n"
+                                        "- Une planche à découper en bois de qualité avec un design unique \n"
+                                        "- Un kit pour préparer des plats spécifiques (ex. kit à sushi, kit à barbecue) \n"
+                                        "- Un livre de recettes sur son type de cuisine préféré (ex. cuisine italienne, asiatique) \n"
+                                        "- Un cours de cuisine en ligne ou en présentiel avec un chef professionnel \n"
+                                        "- Une cocotte ou un faitout en fonte de haute qualité")))
+
+    @Rule(Cadeau(age="41_60ans", sexe="masculin", interest="livres", occasion="anniversaire"))
+    def homme_anniversaire_livres(self):
+        self.declare(Resultat(result=("Pour un homme aimant les livres, voici des idées de cadeaux pour son anniversaire : \n"
+                                        "- Une collection complète de son auteur préféré ou un livre rare \n"
+                                        "- Un abonnement à un service de lecture numérique (ex. Kindle Unlimited, Audible) \n"
+                                        "- Un marque-page personnalisé ou artisanal \n"
+                                        "- Un livre sur un sujet qui le passionne (histoire, science, voyage, philosophie) \n"
+                                        "- Un coffret de beaux livres illustrés sur l'art, la photographie, ou la nature \n"
+                                        "- Un agenda ou carnet de notes élégant pour un amateur de lecture et d'écriture \n"
+                                        "- Une liseuse électronique (si elle n'est pas déjà en sa possession) \n")))
+
+    @Rule(Cadeau(age="41_60ans", sexe="masculin", interest="jeux_de_société", occasion="anniversaire"))
+    def homme_anniversaire_jeux_de_société(self):
+        self.declare(Resultat(result=("Pour un homme aimant les jeux de société, voici des idées de cadeaux pour son anniversaire : \n"
+                                        "- Une version premium ou collector de son jeu de société préféré \n"
+                                        "- Un nouveau jeu de société tendance ou classique (ex. Catan, Carcassonne, Azul) \n"
+                                        "- Un jeu d'échecs ou de dames en bois sculpté pour un style élégant \n"
+                                        "- Un ensemble de jeux de société pour deux joueurs, parfait pour des soirées à deux \n"
+                                        "- Une table de jeux avec des compartiments pour ranger les accessoires \n"
+                                        "- Un livre ou guide sur les stratégies pour les jeux qu'il aime \n"
+                                        "- Des accessoires personnalisés comme des dés uniques ou des pions en métal")))
+
+    @Rule(Cadeau(age="41_60ans", sexe="masculin", interest="mode", occasion="anniversaire"))
+    def homme_anniversaire_mode(self):
+        self.declare(Resultat(result=("Pour un homme aimant la mode, voici des idées de cadeaux pour son anniversaire : \n"
+                                        "- Une montre élégante adaptée à son style (classique, moderne ou sportif) \n"
+                                        "- Un ensemble de cravates, nœuds papillon ou boutons de manchette haut de gamme \n"
+                                        "- Une chemise sur mesure ou une veste en cuir de qualité \n"
+                                        "- Des chaussures tendance, comme des mocassins, sneakers ou bottines \n"
+                                        "- Un portefeuille ou une ceinture en cuir véritable \n"
+                                        "- Une sacoche pour un look chic et pratique \n"
+                                        "- Un abonnement à une box de vêtements ou accessoires de mode \n"
+                                        "- Un parfum raffiné qui reflète sa personnalité")))
+
+    #homme------------------------------------------------------------------------------------------------------------------
+    #retraite
+    @Rule(Cadeau(age="41_60ans", sexe="masculin", interest="jardinage", occasion="retraite"))
+    def homme_retraite_jardinage(self):
+        self.declare(Resultat(result=("Pour un homme aimant le jardinage, voici des idées de cadeaux pour sa retraite : \n"
+                                        "- Un livre spécialisé sur le jardinage ou l'horticulture \n"
+                                        "- Des graines de plantes ou d'arbres à cultiver dans son jardin \n"
+                                        "- Un système d'arrosage automatique ou un tuyau extensible \n"
+                                        "- Un banc de jardin confortable ou une table de pique-nique \n"
+                                        "- Une serre de jardin pour cultiver des plantes délicates ou des légumes \n"
+                                        "- Des accessoires décoratifs pour le jardin comme des statues, des lanternes ou des guirlandes lumineuses \n"
+                                        "- Un kit de culture en intérieur pour cultiver des herbes fraîches toute l'année.")))
+
+    @Rule(Cadeau(age="41_60ans", sexe="masculin", interest="décoration", occasion="retraite"))
+    def homme_retraite_décoration(self):
+        self.declare(Resultat(result=("Pour un homme aimant la décoration, voici des idées de cadeaux pour sa retraite : \n"
+                                        "- Des cadres photo personnalisés pour afficher des souvenirs précieux \n"
+                                        "- Un terrarium ou un ensemble de plantes décoratives d'intérieur \n"
+                                        "- Un diffuseur d'huiles essentielles au design épuré pour l'aromathérapie \n"
+                                        "- Un tapis élégant pour ajouter une touche de confort et de style \n"
+                                        "- Un porte-revues ou un organisateur mural pratique et esthétique \n"
+                                        "- Une lampe ou un luminaire intelligent contrôlé par une application \n"
+                                        "- Des accessoires de décoration sur un thème qu'il apprécie (nature, voyages, minimalisme, etc.).")))
+
+    @Rule(Cadeau(age="41_60ans", sexe="masculin", interest="voyage", occasion="retraite"))
+    def homme_retraite_voyage(self):
+        self.declare(Resultat(result=("Pour un homme aimant le voyage, voici des idées de cadeaux pour sa retraite : \n"
+                                        "- Un abonnement à une box de découverte culturelle (ex. cuisine du monde) \n"
+                                        "- Des gadgets pratiques comme une batterie externe puissante ou un tracker GPS \n"
+                                        "- Une caméra d'action ou un drone compact pour immortaliser ses voyages \n"
+                                        "- Une expérience de voyage unique comme un vol en montgolfière ou une excursion \n"
+                                        "- Des accessoires pour voyager en toute sérénité, comme des écouteurs anti-bruit ou un masque de sommeil \n"
+                                        "- Un abonnement à une application de voyage ou à un magazine spécialisé dans le tourisme.")))
+
+    @Rule(Cadeau(age="41_60ans", sexe="masculin", interest="cuisine", occasion="retraite"))
+    def homme_retraite_cuisine(self):
+        self.declare(Resultat(result=("Pour un homme aimant la cuisine, voici des idées de cadeaux pour sa retraite : \n"
+                                        "- Des épices rares ou un coffret d'huiles d'olive aromatisées \n"
+                                        "- Un appareil de cuisine innovant comme un robot multifonction ou une machine sous vide \n"
+                                        "- Un abonnement à une box culinaire pour découvrir de nouveaux produits chaque mois \n"
+                                        "- Un thermomètre de cuisine numérique ou un chalumeau de cuisine pour les finitions \n"
+                                        "- Des moules ou ustensiles pour des pâtisseries créatives \n"
+                                        "- Un set de verres à vin ou un décanteur pour accompagner ses repas \n"
+                                        "- Une pierre à pizza ou un four à pizza portable pour des soirées conviviales.")))
+
+    @Rule(Cadeau(age="41_60ans", sexe="masculin", interest="livres", occasion="retraite"))
+    def homme_retraite_livres(self):
+        self.declare(Resultat(result=("Pour un homme aimant les livres, voici des idées de cadeaux pour sa retraite : \n"
+                                        "- Une lampe de lecture flexible et rechargeable pour lire confortablement la nuit \n"
+                                        "- Un abonnement à une box livresque qui propose des livres surprises chaque mois \n"
+                                        "- Un atlas ou un guide de voyage pour inspirer ses prochaines aventures \n"
+                                        "- Un livre avec des dédicaces ou des éditions limitées \n"
+                                        "- Une étagère de design original pour mettre en valeur sa collection de livres \n"
+                                        "- Des livres de développement personnel ou de business, s'il s'y intéresse \n"
+                                        "- Une carte cadeau dans sa librairie préférée pour qu'il choisisse lui-même ses lectures.")))
+
+    @Rule(Cadeau(age="41_60ans", sexe="masculin", interest="jeux_de_société", occasion="retraite"))
+    def homme_retraite_jeux_de_société(self):
+        self.declare(Resultat(result=("Pour un homme aimant les jeux de société, voici des idées de cadeaux pour sa retraite : \n"
+                                        "- Une boîte de rangement ou un organiseur pour ses jeux de société \n"
+                                        "- Un abonnement à des événements ou soirées de jeux dans sa région \n"
+                                        "- Une box découverte mensuelle de nouveaux jeux de société \n"
+                                        "- Des jeux de cartes modernes ou classiques (ex. Uno, 7 Wonders, Hanabi) \n"
+                                        "- Un tapis de jeu ou une nappe thématique pour des parties immersives \n"
+                                        "- Une application compagnon pour jeux de société, si disponible pour ses jeux favoris \n"
+                                        "- Des extensions pour ses jeux existants, offrant de nouvelles aventures et défis.")))
+
+    @Rule(Cadeau(age="41_60ans", sexe="masculin", interest="mode", occasion="retraite"))
+    def homme_retraite_mode(self):
+        self.declare(Resultat(result=("Pour un homme aimant la mode, voici des idées de cadeaux pour sa retraite : \n"
+                                        "- Un sac à main de créateur ou une marque tendance \n"
+                                        "- Une paire de lunettes de soleil design ou des lunettes à monture stylée \n"
+                                        "- Un ensemble de soins pour barbe ou cheveux, pour compléter son style \n"
+                                        "- Un foulard ou une écharpe de marque pour les saisons fraîches \n"
+                                        "- Une carte cadeau pour une boutique de mode qu'il aime \n"
+                                        "- Des accessoires modernes, comme une casquette, un chapeau ou une écharpe trendy \n"
+                                        "- Une consultation avec un styliste personnel ou une session de shopping assistée.")))
+
